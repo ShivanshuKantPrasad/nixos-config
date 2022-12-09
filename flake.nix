d7768463976e0059a06c34a1aba7c6aac65ab876
Shivanshu Kant Prasad <shivanshukantprasad@gmail.com>
2022-12-09 18:05:04 +0530

    Initial Commit

---
+++ flake.nix
@@ -0,0 +1,43 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    home-manager = {
+      url = github:nix-community/home-manager;
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    nix-doom-emacs.url = "github:nix-community/nix-doom-emacs";
+
+  };
+
+  outputs = { self, nixpkgs, home-manager, nix-doom-emacs, ... }: 
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs {
+        inherit system;
+	config.allowUnfree = true;
+      };
+
+      lib = nixpkgs.lib;
+    in {
+      nixosConfigurations = {
+        shivanshu = lib.nixosSystem {
+	  inherit system;
+	  modules = [ 
+	    ./configuration.nix 
+
+	    home-manager.nixosModules.home-manager {
+	      home-manager.useGlobalPkgs = true;
+	      home-manager.useUserPackages = true;
+	      home-manager.users.shivanshu =  lib.mkMerge [ 
+                nix-doom-emacs.hmModule
+                ./home.nix 
+              ];
+	    }
+	  ];
+	};
+      };
+    };
+}
---
+++ home.nix
@@ -0,0 +1,50 @@
+{ config, pkgs, ... }:
+
+{
+  #xdg.configFile."fish/functions/rust.fish".source = ./.config/fish/functions/rust.fish;
+
+  home = {
+ 
+    file = {
+      ".intellimacs".source = pkgs.fetchFromGitHub {
+        owner = "MarcoIeni";
+        repo = "intellimacs";
+        rev = "116e566bafb4c7fe9a2962a746281053e59b1f49";
+        sha256 = "0agz51awrq029r8khbg4glwmnz9f1ylk051gbgp5lrmnqkbag282";
+      }; 
+      
+      ".ideavimrc".source = ./.ideavimrc;
+    };
+
+
+    username = "shivanshu";
+    homeDirectory = "/home/shivanshu";
+
+    packages = with pkgs; [
+      bmon
+      usbutils
+      jetbrains.rider
+      pciutils
+      lshw
+      libreoffice
+      cmake
+      gcc
+      gnumake
+      update-nix-fetchgit
+   ];
+
+    stateVersion = "22.11";
+
+  };
+
+  programs = {
+    home-manager.enable = true;
+    bat.enable = true;
+    exa.enable = true;
+    fish.enable = true;
+    doom-emacs = {
+      enable = true;
+      doomPrivateDir = ./.config/doom;
+    };
+  };
+}
