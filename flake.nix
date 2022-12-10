adb2540c9e774dcc57ed16eca823897b406fb23b
Shivanshu Kant Prasad <shivanshukantprasad@gmail.com>
2022-12-10 11:32:19 +0530

    Formatting

--- flake.nix
+++ flake.nix
@@ -4,7 +4,7 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     home-manager = {
-      url = github:nix-community/home-manager;
+      url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
@@ -12,31 +12,30 @@
 
   };
 
-  outputs = { self, nixpkgs, home-manager, ... }: 
+  outputs = { self, nixpkgs, home-manager, ... }:
     let
       system = "x86_64-linux";
       pkgs = import nixpkgs {
         inherit system;
-	config.allowUnfree = true;
+        config.allowUnfree = true;
       };
 
       lib = nixpkgs.lib;
     in {
       nixosConfigurations = {
         shivanshu = lib.nixosSystem {
-	  inherit system;
-	  modules = [ 
-	    ./configuration.nix 
+          inherit system;
+          modules = [
+            ./configuration.nix
 
-	    home-manager.nixosModules.home-manager {
-	      home-manager.useGlobalPkgs = true;
-	      home-manager.useUserPackages = true;
-	      home-manager.users.shivanshu = {
-                imports =  [ ./home.nix ];
-              };
-	    }
-	  ];
-	};
+            home-manager.nixosModules.home-manager
+            {
+              home-manager.useGlobalPkgs = true;
+              home-manager.useUserPackages = true;
+              home-manager.users.shivanshu = { imports = [ ./home.nix ]; };
+            }
+          ];
+        };
       };
     };
 }
--- home.nix
+++ home.nix
@@ -2,28 +2,18 @@
 
 {
   #xdg.configFile."fish/functions/rust.fish".source = ./.config/fish/functions/rust.fish;
-  #xdg.configFile."emacs".source = pkgs.fetchFromGitHub {
-  #  owner = "doomemacs";
-  #  repo = "doomemacs";
-  #  rev = "d5ccac5d71c819035fa251f01d023b3f94b4fba4";
-  #  sha256 = "1hrhh3fa98nc9dc1a4x7slakmf3gfrqrcx4d4vg65rd8rb9wn37c";
-  #}; 
-
-  #xdg.configFile."doom".source = ./.config/doom; 
-
   home = {
- 
+
     file = {
       ".intellimacs".source = pkgs.fetchFromGitHub {
         owner = "MarcoIeni";
         repo = "intellimacs";
         rev = "116e566bafb4c7fe9a2962a746281053e59b1f49";
         sha256 = "0agz51awrq029r8khbg4glwmnz9f1ylk051gbgp5lrmnqkbag282";
-      }; 
-      
+      };
+
       ".ideavimrc".source = ./.ideavimrc;
     };
-
 
     username = "shivanshu";
     homeDirectory = "/home/shivanshu";
@@ -41,8 +31,9 @@
       gcc
       gnumake
       update-nix-fetchgit
-      ((emacsPackagesFor emacsNativeComp).emacsWithPackages (epkgs: [ epkgs.vterm ]))
-   ];
+      ((emacsPackagesFor emacsNativeComp).emacsWithPackages
+        (epkgs: [ epkgs.vterm ]))
+    ];
 
     stateVersion = "22.11";
 
