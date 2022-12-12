a7c1184cc08765da884b0d6dd1afb8b5d0b17526
Shivanshu Kant Prasad <shivanshukantprasad@gmail.com>
2022-12-12 22:15:36 +0530

    waydroid

--- configuration.nix
+++ configuration.nix
@@ -152,6 +152,11 @@
     };
   };
 
+  virtualisation = {
+    waydroid.enable = true;
+    lxd.enable = true;
+  };
+
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
