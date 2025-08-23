38d0318b574bed47c1bcf85227f491e1b96cfc42
Shivanshu Kant Prasad <shivanshukantprasad@gmail.com>
2025-08-23 10:10:03 +0530

    Revert "Add winapps"
    This reverts commit bbb20dadc3ce739b4e11427bfe4cfe12ebb012b0.

--- configuration.nix
+++ configuration.nix
@@ -197,8 +197,6 @@
   };
 
   environment.systemPackages = with pkgs; [
-    inputs.winapps.packages."${pkgs.system}".winapps
-    inputs.winapps.packages."${pkgs.system}".winapps-launcher # optional
     config.boot.kernelPackages.perf
     kdePackages.qtmultimedia
     sddm-astronaut
